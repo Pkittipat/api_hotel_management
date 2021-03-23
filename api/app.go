0ebc5d769f9df3cd0c021d9269f4ee791a2e13b0
Pkittipat <kittipat.phongsak@gmail.com>
2021-03-23 17:29:08 +0700

    Add route hotels for request list of hotels

--- api/app.go
+++ api/app.go
@@ -1,10 +1,14 @@
 package api
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"github.com/gorilla/mux"
 	"hotel_management/users"
+	"hotel_management/hotels"
+	"hotel_management/database"
+	"hotel_management/utils"
 	"encoding/json"
 )
 
@@ -13,7 +17,8 @@ func Run() {
 	router.HandleFunc("/api/users/authenticate", AuthenticateUser).Methods("POST")
 	router.HandleFunc("/api/users/signup", RegistrationUser).Methods("POST")
 	router.HandleFunc("/api/users/me", GetUser).Methods("GET")
-
+	router.HandleFunc("/api/hotels", GetListHotels).Queries().Methods("GET")
+	fmt.Println("App is running on port :8000")
 	log.Fatal(http.ListenAndServe(":8000", router));
 }
 
@@ -38,4 +43,29 @@ func GetUser(w http.ResponseWriter, r *http.Request) {
 	auth := r.Header.Get("Authorization")
 	user := users.GetUser(auth)
 	json.NewEncoder(w).Encode(&user)
+}
+
+func GetListHotels(w http.ResponseWriter, r *http.Request) {
+	hotels := &[]hotels.ResponseHotel{}
+	queryset := database.DB.Table("hotels").Select("id, display_name, description, price")
+
+	search_name := r.FormValue("search_name")
+	search_description := r.FormValue("search_desc")
+
+	if search_name != "" {
+		value := fmt.Sprintf("%%%s%%", r.FormValue("search_name"))
+		queryset.Where("display_name ILIKE ?", value)
+	}
+
+	if search_description != "" {
+		value := fmt.Sprintf("%%%s%%", r.FormValue("search_desc"))
+		queryset.Where("description ILIKE ?", value)
+	}
+	
+	queryset.Find(&hotels)
+	if queryset.Error != nil {
+		json.NewEncoder(w).Encode(utils.HandleResponse("Not found", 404))
+		return
+	}
+	json.NewEncoder(w).Encode(&hotels)
 }
--- migrations/migrations.go
+++ migrations/migrations.go
@@ -2,12 +2,23 @@ package migrations
 
 import (
 	"hotel_management/users"
+	"hotel_management/hotels"
 	"hotel_management/database"
 )
+
+func createMockHotels() {
+	hotels := &[2]hotels.Hotel{
+		{DisplayName: "The Eight Serviced Apartment", Description: "Mahachai, Mueang Samut Sakhon District, Samut Sakhon 74000•092 470 2888", Price: 851},
+		{DisplayName: "Thongchen Residence", Description: "Contemporary serviced apartments with kitchenettes, flat-screens & free Wi-Fi, as well as parking.", Price: 850},
+	}
+	database.DB.Create(&hotels)
+}
 
 func Migrate() {
 	user := users.User{}
 	account := users.Account{}
+	hotel := hotels.Hotel{}
 	
-	database.DB.AutoMigrate(&user, &account)
+	database.DB.AutoMigrate(&user, &account, &hotel)
+	// createMockHotels()
 }
