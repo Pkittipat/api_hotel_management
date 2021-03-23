0c2391df8d7d35640dac8c7a3087eb1381622c50
Pkittipat <kittipat.phongsak@gmail.com>
2021-03-23 21:36:53 +0700

    Add location serializer and modify route infohotel for response hotel location

--- hotels/serializers.go
+++ hotels/serializers.go
@@ -1,8 +1,21 @@
 package hotels
 
+type ResponseLocation struct {
+	Latitude *float64
+	Longitude *float64
+}
+
 type ResponseHotel struct {
 	ID	uint
 	DisplayName string
 	Description string
 	Price float64
+}
+
+type ResponseInfoHotel struct {
+	ID	uint
+	DisplayName string
+	Description string
+	Price float64
+	Location ResponseLocation
 }
