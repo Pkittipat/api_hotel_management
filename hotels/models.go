b1c105a5e1fea2c9bd9b3871774fd76544fabe46
Pkittipat <kittipat.phongsak@gmail.com>
2021-03-23 20:42:03 +0700

    Add model or struct of location for migrate database

--- hotels/models.go
+++ hotels/models.go
@@ -5,10 +5,18 @@ import (
 	"hotel_management/bookings"
 )
 
+type Location struct {
+	*gorm.Model
+	Latitude *float64
+	Longitude *float64
+	Hotel Hotel
+}
+
 type Hotel struct {
 	*gorm.Model
 	DisplayName string
 	Description string
 	Price float64
+	LocationID *uint
 	Bookings []bookings.Booking
 }
