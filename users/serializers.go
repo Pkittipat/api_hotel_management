6bac9bfee8ebafa11840431eb4f1bfc8cf0bcf69
Pkittipat <kittipat.phongsak@gmail.com>
2021-03-23 23:14:38 +0700

    Add validator authentication request data

--- users/serializers.go
+++ users/serializers.go
@@ -3,6 +3,7 @@ package users
 import (
 	"time"
 	"hotel_management/hotels"
+	"hotel_management/utils"
 )
 
 type ResponseUser struct {
@@ -33,6 +34,7 @@ type AuthenticateUserSerializer struct {
 type SignupUserSerializer struct {
 	Username string
 	Password string
+	ConfirmPassword string
 	Email *string
 	Account RequestAccount
 }
@@ -42,4 +44,31 @@ type ResponseBooking struct {
 	ID uint
 	User ResponseUser
 	Hotel hotels.ResponseHotel
+}
+
+func validateUsernameAndPassword(username, password string) map[string]interface{} {
+	if username == "" {
+		return utils.HandleResponse("username field is required", 400)
+	} else if password == "" {
+		return utils.HandleResponse("password field is required", 400)
+	}
+	return nil
+}
+
+func (self *AuthenticateUserSerializer) Validate() map[string]interface{} {
+	return validateUsernameAndPassword(self.Username, self.Password)
+}
+
+
+func (self *SignupUserSerializer) Validate() map[string]interface{} {
+	err := validateUsernameAndPassword(self.Username, self.Password)
+	if err != nil {
+		return err
+	}
+
+	if self.Password != self.ConfirmPassword {
+		return utils.HandleResponse("Password does not match", 400)
+	}
+
+	return nil
 }
