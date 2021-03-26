4015189c3f3ce3f93759844967a1b2afd7213106
Pkittipat <kittipat.phongsak@gmail.com>
2021-03-26 18:17:50 +0700

    Add unit testing with booking validate data request create booking

--- bookings/booking_test.go
+++ bookings/booking_test.go
@@ -2,23 +2,22 @@ package bookings
 
 import (
 	"testing"
+	"hotel_management/utils"
 )
 
 func TestValidateErrorNoDataRequest(t *testing.T) {
-	req := CreateBookingSerializer{}
+	req := CreateBookingSerializer{HotelID: 0}
 	if req.Validate() == nil {
-		t.Errorf("Validate() fail")
+		t.Errorf("Validate() failed, expected %v, got %v", utils.HandleResponse("Hotel Id field is required", 400), req.Validate())
 	} else {
 		t.Logf("Validate() success")
 	}
 }
 
 func TestDataRequestIsValid(t *testing.T) {
-	req := CreateBookingSerializer{
-		HotelID: 1,
-	}
+	req := CreateBookingSerializer{HotelID: 1}
 	if req.Validate() != nil {
-		t.Errorf("Validate() fail")
+		t.Errorf("Validate() failed, expexted %v, got %v", map[string]interface{}{}, req.Validate())
 	} else {
 		t.Logf("Validate() success")
 	}
